b597bcdf81d645589c620488754effa3d7f4cc89
Dmitrii <74898966+Dmitrii-Kolikov@users.noreply.github.com>
2021-01-26 23:35:49 +0700

    Update ClientDAO.java

--- src/main/java/spring/mvc/dao/ClientDAO.java
+++ src/main/java/spring/mvc/dao/ClientDAO.java
@@ -9,4 +9,8 @@ public interface ClientDAO {
     public List<Client> getAllClients();
 
     public void saveClient(Client client);
+
+    public Client getClient(int id);
+
+    public void deleteClient(int id);
 }
